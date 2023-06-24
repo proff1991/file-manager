0ee8cb8abb213c182bbabb560d69793bbb78b9f3
proff1991 <parkhomenko.sitel@gmail.com>
2023-06-24 18:24:18 +0300

    модуль

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-// for(let elem of process.argv){
-//     console.log(elem)
-// }
-// process.username = "proff"
-// console.log(process.username)
+import { EOL } from "os";
 
 const checkUsersName = "--username="
 const unknownUsersName = "noname"
 
 let username = process.argv.slice(2).find( arg => arg.includes(checkUsersName));
 
-if (username && username.length > checkUsersName.length) {
+if (username && username.length > checkUsersName) {
 
     process.env.username = username.split(checkUsersName)[1]
 
