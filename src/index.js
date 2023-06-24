ff5fa38e44422ab69e0789f6b93aaa7553b600c7
proff1991 <parkhomenko.sitel@gmail.com>
2023-06-24 18:21:27 +0300

    username

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-for(let elem of process.argv){
-    console.log(elem)
+// for(let elem of process.argv){
+//     console.log(elem)
+// }
+// process.username = "proff"
+// console.log(process.username)
+
+const checkUsersName = "--username="
+const unknownUsersName = "noname"
+
+let username = process.argv.slice(2).find( arg => arg.includes(checkUsersName));
+
+if (username && username.length > checkUsersName.length) {
+
+    process.env.username = username.split(checkUsersName)[1]
+
+} else {
+
+    process.env.username = unknownUsersName
+
 }
+
+
+console.log(process.env.username)
