1b3c4b48625b0fe77ffc69386a391e8849c2b4de
proff1991 <parkhomenko.sitel@gmail.com>
2023-06-25 23:26:18 +0300

    Сделал, теперь копируются файлы!

--- src/files/actionCopy.js
+++ src/files/actionCopy.js
@@ -1,18 +1,21 @@
 import { createReadStream as fsCreateReadStream, createWriteStream as fsCreateWriteStream } from 'fs'
 import { pipeline as streamPipeline } from 'stream/promises'
-import { join as pathJoin, isAbsolute as pathIsAbsolute } from "path";
+import { join as pathJoin, isAbsolute as pathIsAbsolute, dirname as pathDirname, parse as pathParse } from "path";
 const errorMessage = "Operation failed"
 
 const actionCopy = async (__dirname, file, newFileName) => {
-    console.log(__dirname)
-    console.log(file)
-    console.log(newFileName)
 
-    let currentFilePath = pathIsAbsolute(file) ? file : pathJoin(__dirname, file)
-    let newFilePath = pathIsAbsolute(newFileName) ? newFileName : pathJoin(pathDirname(currentFilePath), newFileName)
+    let currentFilePath = pathIsAbsolute(file) 
+        ? file 
+        : pathJoin(__dirname, file)
 
-    console.log(currentFilePath)
-    console.log(newFilePath)
+    let newFilePath = pathIsAbsolute(newFileName) 
+        ? newFileName 
+        : pathJoin(pathDirname(currentFilePath), newFileName)
+
+    newFilePath = pathParse(currentFilePath)['base'] == pathParse(newFilePath)['base'] 
+        ? newFilePath
+        : newFilePath + pathParse(currentFilePath)['base']
 
     try {
 
